3b2cd648a290ada33e6ab01308ea42fb04446d9d
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-04-26 14:24:30 +0200

    init

---
+++ precache-manifest.8ccd048e0a23d0106d7ace9eec2394c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0837b796ece2721ed5f",
+    "url": "/css/app.356f6e9b.css"
+  },
+  {
+    "revision": "9a013a2f3d11618d6ae5903cda94d75b",
+    "url": "/img/cart.9a013a2f.svg"
+  },
+  {
+    "revision": "bb67260322f04a0b199ab61008ef3b49",
+    "url": "/img/hero.bb672603.jpg"
+  },
+  {
+    "revision": "72858d5d7787e3db17ebed3e1b11e1a5",
+    "url": "/img/shoes.72858d5d.jpg"
+  },
+  {
+    "revision": "23cbd49c6423426ae5b695055d18d550",
+    "url": "/img/woman-with-scarf.23cbd49c.jpg"
+  },
+  {
+    "revision": "6dc8b4f8dffdfe50222310b672799715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0837b796ece2721ed5f",
+    "url": "/js/app.c90d1866.js"
+  },
+  {
+    "revision": "3145551d944dc8f1da91",
+    "url": "/js/chunk-vendors.af1f060b.js"
+  },
+  {
+    "revision": "6d3d5bf28f11fc1d4541f725e8f1deeb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
